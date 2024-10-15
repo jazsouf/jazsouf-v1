501930b6f7575b190888e86dcb8f6b22723483d3
jazsouf <101473962+jazsouf@users.noreply.github.com>
2024-10-15 15:09:56 +0300

    fix type

--- app/(folio)/projects/[slug]/page.tsx
+++ app/(folio)/projects/[slug]/page.tsx
@@ -29,7 +29,9 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export async function generateStaticParams() {
   const slugs = await getProjectsPaths();
-  return slugs.map((slug: string) => slug);
+  console.log("static params", slugs);
+
+  return slugs.map((slug) => slug.slug);
 }
 
 export default async function ProjectSlugRoute({ params }: Props) {
